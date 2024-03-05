c8d93268cdbe794b5a3eec697e80cfdfdbd9c10b
ineza2 <ineza2ineza@gmail.com>
2024-03-05 14:49:06 +0200

    hibernate 2

--- Revision/src/main/java/app/models/Employee.java
+++ Revision/src/main/java/app/models/Employee.java
@@ -1,12 +1,15 @@
 package app.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String firstName;
 	private String lastName;
